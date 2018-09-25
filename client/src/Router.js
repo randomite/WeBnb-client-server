e4f31822f38b1e2bc257bc135c67132794cd9f8b
Juan Castillo <juan.a.castillo@sjsu.edu>
2018-09-24 19:30:58 -0700

    Commit front-end boilerplate

---
+++ client/src/Router.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+// All pages here
+import Landing from './components/pages/Landing'
+
+
+const Router = () => (
+  <main>
+    <Switch>
+      <Route path='/' component={Landing}/>
+
+    </Switch>
+  </main>
+)
+
+export default Router
---
+++ client/src/components/pages/Landing.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import Header from '../ui/Header'
+
+const backgroundStyle = {
+  backgroundImage: "url(http://i.imgur.com/Kp076D5.jpg)"
+};
+
+class Landing extends React.Component {
+  render() {
+    return (
+      <div className="landing" style={backgroundStyle}>
+        <Header>HEADER</Header>
+        <div>SEARCH</div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Landing);
