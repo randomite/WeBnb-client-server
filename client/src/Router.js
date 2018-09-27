9fb2d994b2616483d944f0812740cd7beebba18d
derianlemus95 <42625765+derianlemus95@users.noreply.github.com>
2018-09-27 14:46:54 -0700

    Updated Router.js
    Now it has paths for privacy and terms

--- client/src/Router.js
+++ client/src/Router.js
@@ -3,12 +3,15 @@ import { Route, Switch } from 'react-router-dom';
 
 // All pages here
 import Landing from './components/pages/Landing'
-
+import Privacy from './components/pages/Privacy'
+import Terms from './components/pages/Terms'
 
 const Router = () => (
   <main>
     <Switch>
-      <Route path='/' component={Landing}/>
+      <Route exact path='/' component={Landing}/>
+      <Route path='/privacy' component={Privacy}/>
+      <Route path='/terms' component={Terms}/>
 
     </Switch>
   </main>
