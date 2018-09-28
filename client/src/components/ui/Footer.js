ccaffd7b9ffd0aabf31a81b937a618b05fde5909
Daanyaal Ahad Saeed <daanyaal.saeed@sjsu.edu>
2018-09-27 17:52:47 -0700

    Npm installed Materials Icon pack

--- client/src/components/ui/Footer.js
+++ client/src/components/ui/Footer.js
@@ -1,9 +1,23 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 
+import Icon from "@material-ui/core/Icon";
+
+const styleFooter = {
+  position: "fixed",
+  bottom: 10,
+  right: 10
+};
+
 class Footer extends React.Component {
   render() {
-    return <footer style={{ position: "fixed" }}> </footer>;
+    return (
+      <button style={styleFooter}>
+        <Icon class="material-icons md-18">language</Icon>
+        <Icon class="material-icons md-18">help</Icon>
+        Terms, Privacy, Currency & More
+      </button>
+    );
   }
 }
 
