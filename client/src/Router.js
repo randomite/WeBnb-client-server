af8c5a6bcc55bc570fa7408efefc0da793067b23
louievuitton <stevenlouie96@gmail.com>
2018-09-28 20:08:33 -0700

    Updated Router.js

--- client/src/Router.js
+++ client/src/Router.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 // All pages here
-import Landing from './components/pages/Landing'
+import Landing from './components/pages/Landing';
+import AboutUs from './components/pages/aboutUs';
 
 
 const Router = () => (
   <main>
     <Switch>
+      <Route path ='/AboutUs' component={AboutUs}/>
       <Route path='/' component={Landing}/>
-
     </Switch>
   </main>
 )
 
-export default Router
+export default Router;
