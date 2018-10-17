ea2a782c56a5f3279cc58d6b88865b51e0e92669
Delta09 <Delta09@users.noreply.github.com>
2018-10-16 19:35:07 -0700

    Update Privacy.js

--- client/src/components/pages/Privacy.js
+++ client/src/components/pages/Privacy.js
@@ -13,58 +13,58 @@ class Privacy extends React.Component {
           <body>
             <div className="body">
               <div className="title">
-                <h1>Webnb Privacy Policy</h1>
+                <h1>WeBnb Privacy Policy</h1>
               </div>
 
               <p>Last Updated: March 17,2018!</p>
-              <p>Thank you for using Webnb!</p>
+              <p>Thank you for using WeBnb!</p>
               <p>
                 This Privacy Policy describes how we collect, use, process, and
                 disclose your information, including personal information, in
-                conjunction with your access to and use of the Webnb Platform
+                conjunction with your access to and use of the WeBnb Platform
                 and the Payment Services.
               </p>
 
               <p>
                 If you see an undefined term in this Privacy Policy (such as
-                “Listing” or “Webnb Platform”), it has the same definition as in
+                “Listing” or “WeBnb Platform”), it has the same definition as in
                 our Terms of Service.
               </p>
 
               <p>
-                When this policy mentions “Webnb,” “we,” “us,” or “our,” it
-                refers to the Webnb company that is responsible for your
+                When this policy mentions “WeBnb,” “we,” “us,” or “our,” it
+                refers to the WeBnb company that is responsible for your
                 information under this Privacy Policy (the “Data Controller”).
               </p>
               <ul>
                 <li>
                   If your Country of Residence is the United States, the Data
-                  Controller is Webnb, Inc.
+                  Controller is WeBnb, Inc.
                 </li>
                 <li>
                   If your Country of Residence is outside of the United States,
                   the People’s Republic of China which for purposes of this
                   Privacy Policy does not include Hong Kong, Macau and Taiwan
-                  (“China”) and Japan, the Data Controller is Webnb Ireland UC
-                  (“Webnb Ireland”).
+                  (“China”) and Japan, the Data Controller is WeBnb Ireland UC
+                  (“WeBnb Ireland”).
                 </li>
                 <li>
                   If your Country of Residence is Japan, the Data Controller is
-                  Webnb Global Services Limited (“Webnb GSL”), except where you
+                  WeBnb Global Services Limited (“WeBnb GSL”), except where you
                   book a Host Service located outside of Japan or create a
                   Listing located outside of Japan, in which case the Data
-                  Controller is Webnb Ireland for that transaction.
-                  Additionally, if you reside in Japan, Webnb Ireland will be
+                  Controller is WeBnb Ireland for that transaction.
+                  Additionally, if you reside in Japan, WeBnb Ireland will be
                   the Data Controller for all bookings confirmed prior to June
                   13, 2018 at 3PM UTC.
                 </li>
               </ul>
               <p>
                 This Privacy Policy also applies to the Payment Services
-                provided to you by Webnb Payments pursuant to the Payments Terms
+                provided to you by WeBnb Payments pursuant to the Payments Terms
                 of Service (“Payments Terms”). When using the Payment Services,
                 you will be also providing your information, including personal
-                information, to one or more Webnb Payments entities, which will
+                information, to one or more WeBnb Payments entities, which will
                 also be the Data Controller (the "Payments Data Controller") of
                 your information related to the Payment Services, generally
                 depending on your Country of Residence.
@@ -72,8 +72,8 @@ class Privacy extends React.Component {
               <ul>
                 <li>
                   If your Country of Residence is the United States, the
-                  Payments Data Controller is Webnb Payments, Inc. (a subsidiary
-                  of Webnb, Inc.).
+                  Payments Data Controller is WeBnb Payments, Inc. (a subsidiary
+                  of WeBnb, Inc.).
                 </li>
                 <li>
                   If your Country of Residence is China the Payments Data
