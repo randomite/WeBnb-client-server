42e0ad66ead52df37c64ec9f6b3665bf66305add
Juan Castillo <juan.a.castillo@sjsu.edu>
2018-10-23 23:07:10 -0700

    Refactor gallery to be more responsive

--- client/src/components/ui/Gallery.js
+++ client/src/components/ui/Gallery.js
@@ -1,29 +1,46 @@
-import React from 'react'
-import {Button} from '@material-ui/core'
+import React from "react";
+import { Button } from "@material-ui/core";
 
-export default class Gallery extends React.Component{
-
-    renderImages = () =>{
-
-    }
-    render(){
-        return <div className='gallery'>
-            <div className='gallery_main'>
-                <img src={this.props.images[0].src}/>
+export default class Gallery extends React.Component {
+  render() {
+    return (
+      <div className="gallery">
+        <div
+          className="gallery_main"
+          style={{ backgroundImage: `url(${this.props.images[0].src}` }}
+        />
+        <div className="gallery_extra">
+          <div className="col2">
+            <div>
+              <div
+                style={{ backgroundImage: `url(${this.props.images[1].src}` }}
+              />
             </div>
-            <div className='gallery_extra'>
-                <div><img src={this.props.images[1].src}/></div>
-                <div>
-                    <img src={this.props.images[2].src}/>
-                </div>
+            <div>
+              <div
+                style={{ backgroundImage: `url(${this.props.images[2].src}` }}
+              />
             </div>
-            <div className='gallery_extra'>
-                <div><img src={this.props.images[3].src}/></div>
-                <div><img src={this.props.images[4].src}/></div>
+          </div>
+          <div className="col2">
+            <div>
+              <div
+                style={{ backgroundImage: `url(${this.props.images[3].src}` }}
+              />
             </div>
-            <div className='view_all'>
-                <Button variant='contained' onClick={this.props.onClick}>View all images</Button>
+            <div>
+              <div
+                style={{ backgroundImage: `url(${this.props.images[4].src}` }}
+              />
             </div>
+          </div>
         </div>
-    }
+        <div className="view_all">
+          <Button variant="contained" onClick={this.props.onClick}>
+            View all images
+          </Button>
+        </div>
+      </div>
+    );
+  }
 }
