61941ecffbe8612f48eecf977221cc713b168be5
louievuitton <stevenlouie96@gmail.com>
2018-10-27 21:37:00 -0700

    Update rootReducer.js with counter reducer

--- client/src/redux/rootReducer.js
+++ client/src/redux/rootReducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 // ADD REDUCERS HERE
 import authentication from './reducers/authentication'
 import user from './reducers/user'
+import counter from './reducers/counter';
 
 const rootReducer = combineReducers({
   authentication: authentication,
   user: user,
+  counter: counter
 });
 
 export default rootReducer;
