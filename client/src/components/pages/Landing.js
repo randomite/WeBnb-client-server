c59ae481d301c997d5ff1304a070e0b913bd2f58
derianlemus95 <42625765+derianlemus95@users.noreply.github.com>
2018-10-28 02:15:44 -0700

    Current landing page

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -3,6 +3,8 @@ import { withRouter } from "react-router-dom";
 import Header from "../ui/Header";
 import Footer from "../ui/Footer";
 import imgurl from "../../img/home1.jpg";
+import TripInfoModal from "../ui/TripInfoModal";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 const backgroundStyle = {
   // backgroundImage: "url(http://i.imgur.com/Kp076D5.jpg)"
@@ -10,12 +12,43 @@ const backgroundStyle = {
 };
 
 class Landing extends React.Component {
+  state = {
+    fields: {}
+  };
+
+  onChange = updatedValue => {
+    this.setState({
+      fields: {
+        ...this.state.fields,
+        ...updatedValue
+      }
+    });
+  };
+
+
   render() {
     return (
+      <MuiThemeProvider>
       <div className="landing" style={backgroundStyle}>
         <Header variant='secondary'/>
+
+            <div className= "outterbox">
+              <div className="outterbox1">
+                <div className="outterbox2">
+                  <div className="outterbox3">
+                    <div className="desktopView">
+
+                      <TripInfoModal onChange={fields => this.onChange(fields)}/>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
         <Footer/>
       </div>
+      </MuiThemeProvider>
     );
   }
 }
