9d7148184fa6431c8a0ab154d8b7e69e24a9c438
derianlemus95 <42625765+derianlemus95@users.noreply.github.com>
2018-10-28 02:16:56 -0700

    Add files via upload

---
+++ client/src/components/ui/TripInfoModal.js
@@ -0,0 +1,165 @@
+import React from "react";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import { connect } from "react-redux";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  FormControl,
+  TextField,
+  InputLabel,
+  InputAdornment,
+  IconButton,
+  DialogActions,
+  Button,
+  CircularProgress
+} from "@material-ui/core";
+import OutlinedInput from "@material-ui/core/OutlinedInput/OutlinedInput";
+import FormHelperText from "@material-ui/core/FormHelperText/FormHelperText";
+import validator from "validator";
+import { instance } from "../../Axios";
+
+
+
+
+export default class TripInfoModal extends React.Component {
+  state = {
+    destination: "",
+    destinationError: "",
+    start: "",
+    startError: "",
+    end: "",
+    endError: "",
+    numberofpeople: "",
+    numberofpeopleError: "",
+  };
+
+change = e => {
+  this.props.onChange({ [e.target.name]: e.target.value });
+  this.setState({
+    [e.target.name]: e.target.value
+  });
+};
+
+validate = () => {
+  let isError  =  false;
+  const errors = {
+    destinationError: "",
+    startError: "",
+    endError: "",
+    numberofpeopleError: "",
+  };
+  if(!this.state.destination) {
+    isError = true;
+    errors.destinationError = "Can't be empty"
+  }
+
+    this.setState({
+      ...this.state,
+      ...errors
+    });
+  return isError;
+};
+onSubmit = e =>{
+  e.preventDefault();
+  const err = this.validate();
+  if (!err) {
+  this.setState({
+    destination: "",
+    destinationError: "",
+    start: "",
+    startError: "",
+    end: "",
+    endError: "",
+    numberofpeople: "",
+    numberofpeopleError: ""
+  });
+  this.props.onChange({
+    destination: "",
+    start: "",
+    end: "",
+    numberofpeople: "",
+  });
+  }
+};
+
+render () {
+  return  (
+    <form>
+      <p>Bookings catered for you</p>
+      <TextField
+      name = "destination"
+      variant="outlined"
+      fullWidth
+      label="Where To"
+
+      placeholder="Anywhere"
+      margin="normal"
+      required
+      errrorText= {this.state.destinationError}
+      value={this.state.destination}
+      onChange={e => this.change(e)}
+
+      InputLabelProps={{
+        shrink: true,
+      }}
+      />
+      <br />
+      <TextField
+      name = "start"
+      variant="outlined"
+      fullWidth
+      label="CHECK IN"
+      placeholder="mm/dd/yyyy"
+      margin="normal"
+      required
+        errrorText= {this.state.startError}
+      value={this.state.start}
+      onChange={e => this.change(e)}
+      InputLabelProps={{
+        shrink: true,
+      }}
+      />
+      <TextField
+      name = "end"
+      variant="outlined"
+      fullWidth
+      label="CHECK OUT"
+      placeholder="mm/dd/yyyy"
+      margin="normal"
+      required
+
+      value={this.state.end}
+      onChange={e => this.change(e)}
+      InputLabelProps={{
+        shrink: true,
+      }}
+      />
+
+      <br />
+      <TextField
+      name = "numberofpeople"
+      variant="outlined"
+      fullWidth
+      label="GUESTS"
+      placeholder="1 guest"
+      margin="normal"
+      required
+
+      value={this.state.numberofpeople}
+      onChange={e => this.change(e)}
+      InputLabelProps={{
+        shrink: true,
+      }}
+      />
+      <br />
+      <Button
+        variant="contained"
+        color="secondary"
+
+      onClick={e=> this.onSubmit(e)}>Search</Button>
+    </form>
+  );
+}
+}
