9e90a07c5d9fdfc1cac32e421faff21930f8468e
Juan Castillo <juan.a.castillo@sjsu.edu>
2018-11-01 13:44:43 -0700

    Move Lightbox into Gallery component

--- client/src/components/pages/HotelView.js
+++ client/src/components/pages/HotelView.js
@@ -1,111 +1,49 @@
 import React from "react";
 import Header from "../ui/Header";
-import Lightbox from "react-images";
 import Gallery from "../ui/Gallery";
 import "react-dates/lib/css/_datepicker.css";
 import "react-dates/initialize";
-import moment from "moment";
 import RoomGrid from "../ui/RoomGrid";
 import BookingDetails from "../ui/BookingDetails";
-import instance from '../../Axios'
-
-const datesList = [
-  moment(),
-  moment().add(1, "days"),
-  moment().add(3, "days"),
-  moment().add(9, "days"),
-  moment().add(10, "days"),
-  moment().add(11, "days"),
-  moment().add(12, "days"),
-  moment().add(13, "days")
-];
+import instance from "../../Axios";
 
 const hotel_data = require("./hotel_data");
 
 export default class HotelView extends React.Component {
   constructor() {
     super();
-    this.closeLightbox = this.closeLightbox.bind(this);
-    this.openLightbox = this.openLightbox.bind(this);
-    this.gotoNext = this.gotoNext.bind(this);
-    this.gotoPrevious = this.gotoPrevious.bind(this);
-
-    let temp = hotel_data.rooms.map(room => {
-      return room.image_url
-    })
 
     this.state = {
-      currentImage: 0,
       startDate: null,
       endDate: null,
-      focusedInput: null,
-      photos: temp.reduce(
-        function(accumulator, currentValue) {
-          return accumulator.concat(currentValue);
-        },
-        []
-      )
+      focusedInput: null
     };
 
     //GET DATA FROM BACK END
   }
-  openLightbox(event, obj) {
-    console.log('OPEN LIGHTBOX',obj)
-    this.setState({
-      currentImage: obj,
-      lightboxIsOpen: true
-    });
-  }
-  closeLightbox() {
-    this.setState({
-      currentImage: 0,
-      lightboxIsOpen: false
-    });
-  }
-  gotoPrevious() {
-    this.setState({
-      currentImage: this.state.currentImage - 1
-    });
-  }
-  gotoNext() {
-    this.setState({
-      currentImage: this.state.currentImage + 1
-    });
-  }
+
+  combineRoomImages = () => {
+    return hotel_data.rooms.reduce(function(accumulator, currentValue) {
+      return accumulator.concat(currentValue.images);
+    }, []);
+  };
 
   render() {
-
+    console.log("REDUCED ROOMS", this.combineRoomImages());
     return (
       <div>
         <Header />
         <div className="hotel">
           <div className="gallery">
             <Gallery
-              images={this.state.photos}
-              onClick={(e, index) => {
-                this.openLightbox(e,index);
-              }}
-            />
-            <Lightbox
-              images={this.state.photos}
-              onClose={this.closeLightbox}
-              onClickPrev={this.gotoPrevious}
-              onClickNext={this.gotoNext}
-              currentImage={this.state.currentImage}
-              isOpen={this.state.lightboxIsOpen}
+              hotelImages={hotel_data.images}
+              roomImages={this.combineRoomImages()}
             />
           </div>
           <div className="hotel_details">
             <div className="details">
               <h1>{hotel_data.name}</h1>
               <h5>{hotel_data.address}</h5>
-              {/*<div>*/}
-              {/*<h3>Availability</h3>*/}
-              {/*<div>*/}
-              {/*<AvailableDatesWrapper isDayBlocked={day1 => datesList.some(day2 => isSameDay(day1, day2))}/>*/}
-              {/*<CalendarMonthGrid   initialMonth={moment().add(1,'month')}   numberOfMonths={2} />*/}
-              {/*</div>*/}
-              {/*</div>*/}
               <div>
                 <h3>Available Rooms</h3>
                 <div>
--- client/src/components/ui/Gallery.js
+++ client/src/components/ui/Gallery.js
@@ -1,51 +1,118 @@
 import React from "react";
 import { Button } from "@material-ui/core";
+import Lightbox from "react-images";
+import PropTypes from "prop-types";
 
 export default class Gallery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.closeLightbox = this.closeLightbox.bind(this);
+    this.openLightbox = this.openLightbox.bind(this);
+    this.gotoNext = this.gotoNext.bind(this);
+    this.gotoPrevious = this.gotoPrevious.bind(this);
+
+    console.log("PROPS GALLERY:", props);
+
+    this.state = {
+      currentImage: 0,
+      photos: props.hotelImages.concat(props.roomImages)
+    };
+  }
+  openLightbox(event, obj) {
+    console.log("OPEN LIGHTBOX", obj);
+    this.setState({
+      currentImage: obj,
+      lightboxIsOpen: true
+    });
+  }
+
+  closeLightbox() {
+    this.setState({
+      currentImage: 0,
+      lightboxIsOpen: false
+    });
+  }
+  gotoPrevious() {
+    this.setState({
+      currentImage: this.state.currentImage - 1
+    });
+  }
+  gotoNext() {
+    this.setState({
+      currentImage: this.state.currentImage + 1
+    });
+  }
+
+  randomIntFromInterval = (
+    min,
+    max // min and max included
+  ) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
   render() {
     return (
       <div className="gallery">
         <div
           className="gallery_main"
-          onClick={e => this.props.onClick(e, 0)}
-          style={{ backgroundImage: `url(${this.props.images[0].src}` }}
+          onClick={e => this.openLightbox(e, 0)}
+          style={{ backgroundImage: `url(${this.state.photos[0].src}` }}
         />
         <div className="gallery_extra">
           <div className="col2">
             <div>
               <div
-                onClick={e => this.props.onClick(e, 1)}
-                style={{ backgroundImage: `url(${this.props.images[1].src}` }}
+                onClick={e => this.openLightbox(e, 1)}
+                style={{ backgroundImage: `url(${this.state.photos[1].src}` }}
               />
             </div>
             <div>
               <div
-                onClick={e => this.props.onClick(e, 2)}
-                style={{ backgroundImage: `url(${this.props.images[2].src}` }}
+                onClick={e => this.openLightbox(e, 2)}
+                style={{ backgroundImage: `url(${this.state.photos[2].src}` }}
               />
             </div>
           </div>
           <div className="col2">
             <div>
               <div
-                onClick={e => this.props.onClick(e, 3)}
-                style={{ backgroundImage: `url(${this.props.images[3].src}` }}
+                onClick={e => this.openLightbox(e, 3)}
+                style={{ backgroundImage: `url(${this.state.photos[3].src}` }}
               />
             </div>
             <div>
               <div
-                onClick={e => this.props.onClick(e, 4)}
-                style={{ backgroundImage: `url(${this.props.images[4].src}` }}
+                onClick={e => this.openLightbox(e, 4)}
+                style={{
+                  backgroundImage: `url(${
+                    this.state.photos[
+                      this.randomIntFromInterval(0, this.state.photos.length)
+                    ].src
+                  }`
+                }}
               />
             </div>
           </div>
         </div>
         <div className="view_all">
-          <Button variant="contained" onClick={e => this.props.onClick(e, 0)}>
+          <Button variant="contained" onClick={e => this.openLightbox(e, 0)}>
             View all images
           </Button>
         </div>
+        <Lightbox
+          images={this.state.photos}
+          onClose={this.closeLightbox}
+          onClickPrev={this.gotoPrevious}
+          onClickNext={this.gotoNext}
+          currentImage={this.state.currentImage}
+          isOpen={this.state.lightboxIsOpen}
+        />
       </div>
     );
   }
 }
+
+Gallery.PropTypes = {
+  hotelImages: PropTypes.array,
+  roomImages: PropTypes.array
+};
